f9b133beadba3b13a5046a36b2aa328ac56159fb
Hoang Van Sang <69237271+sanghv2@users.noreply.github.com>
2021-08-27 21:36:12 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "AKIAY4NDH7Q66JH3QGEQ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
